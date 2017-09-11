4ee57be3a5022985a6d0c97c97159c65e73a1db7
Jan Moeller <jan.moeller0@gmail.com>
2017-09-11 12:51:52 +0200

    Read postgres credentials from env

--- config/dev.exs
+++ config/dev.exs
@@ -32,8 +32,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :patrician_server, PatricianServer.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
   database: "patrician_server_dev",
   hostname: "localhost",
   pool_size: 10
